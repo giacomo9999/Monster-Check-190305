cc63f13c5d18ed2b6d62bb693498f6e38c18ea8c
James Gary <gmarchesi888@gmail.com>
2019-03-05 09:30:42 -0500

    Added rudimentary styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Container, Card, Header } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container text>
+        <br />
+        <Header
+          as="h2"
+          textAlign="center"
+          style={{
+            fontSize: "2em",
+            fontWeight: "normal",
+            marginBottom: 0,
+            marginTop: "3em"
+          }}
+        >
+          MonsterCheck CRUD App
+        </Header>
+
+        {/* <Card centered>
+          <Card.Content>
+            <Card.Header>Matthew Harris</Card.Header>
+            <Card.Meta>Co-Worker</Card.Meta>
+            <Card.Description>
+              Matthew is a pianist living in Nashville.
+            </Card.Description>
+          </Card.Content>
+        </Card> */}
+      </Container>
     );
   }
 }
