f6080d0d06f56fe0ce451fa56a90065d9cfcaee7
James Gary <gmarchesi888@gmail.com>
2019-03-06 16:13:15 -0500

    Basic server in place.

--- client/src/components/create.component.js
+++ client/src/components/create.component.js
@@ -1,23 +1,76 @@
 import React, { Component } from "react";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Form, Button } from "semantic-ui-react";
+const uuidv1 = require("uuid/v1");
 
 export default class Create extends Component {
+  state = {
+    regionName: "",
+    id: "",
+    regionType: "",
+    regionDifficulty: 0,
+    regionMonstersAndFreq: []
+  };
+
+  onChangeRegionName(e) {
+    this.setState({ regionName: e.target.value });
+  }
+
+  onChangeRegionType(e) {
+    this.setState({ regionType: e.target.value });
+  }
+
+  onChangeRegionDifficulty(e) {
+    this.setState({ regionDifficulty: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(
+      `The values are: ${this.state.regionName},${this.state.regionType},${
+        this.state.regionDiffculty
+      }`
+    );
+    this.setState({
+      regionName: "",
+      id: "",
+      regionType: "",
+      regionDifficulty: 0,
+      regionMonstersAndFreq: []
+    });
+  }
+
   render() {
     return (
       <Container text>
         <br />
         <Header
-          as="h2"
+          as="h3"
           textAlign="center"
           style={{
-            fontSize: "2em",
+            fontSize: "1.5",
             fontWeight: "normal",
             marginBottom: 0,
-            marginTop: "3em"
+            marginTop: "2em"
           }}
         >
-          Welcome to Create
+          Create New Table
         </Header>
+        <Form>
+          <Form.Field>
+            <label>Region Name</label>
+            <input placeholder="Region Name" />
+          </Form.Field>
+          <Form.Field>
+            <label>Region Type</label>
+            <input placeholder="Region Type" />
+          </Form.Field>
+          <Form.Field>
+            <label>Region Difficulty</label>
+            <input placeholder="Difficulty" />
+          </Form.Field>
+
+          <Button type="submit">Submit</Button>
+        </Form>
       </Container>
     );
   }
